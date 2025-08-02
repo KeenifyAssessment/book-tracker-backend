4d9a8cf6022d517be7b7b7a0c66abebee6f60587
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-08-02 13:05:00 +0800

    fixed the cors settings

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Book Tracker API", version="1.0.0")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # adjust for prod
+    allow_origins=["http://localhost:3000", "https://keenify-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
